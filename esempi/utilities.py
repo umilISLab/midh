176e09c7ab337c86c7dbed6df5fdd00ad5ebf5f0
Alfio Ferrara <alfio.ferrara@unimi.it>
2024-10-09 18:02:58 +0200

    Update hangman

--- esempi/utilities.py
+++ esempi/utilities.py
@@ -5,16 +5,31 @@ def read_words(file_path):
     del(words[-1])
     return words 
 
-def print_template(word, marker=" _ ", character="", space=True):
+def print_template(word, marker=" _ ", character="", space=True, current_guess=None):
+    if current_guess is None:
+        current_guess = "." * len(word)
     template = ""
-    for c in word:
+    for i, c in enumerate(word):
         if c == character:
             if space:
                 template += " {} ".format(c)
             else:
                 template += c
         else:
-            template += marker
+            g = current_guess[i]
+            if g == '.':
+                template += marker
+            else:
+                template += g
     return template
+
+def update_guess(character, word_to_guess, current_guess):
+    new_current_guess = ""
+    for i, c in enumerate(word_to_guess):
+        if c == character:
+            new_current_guess += c
+        else:
+            new_current_guess += current_guess[i]
+    return new_current_guess
             
         
