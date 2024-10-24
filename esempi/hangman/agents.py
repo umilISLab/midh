9209dc73ae8a871ab510d19c45689be5ea339845
Alfio Ferrara <alfio.ferrara@unimi.it>
2024-10-24 10:42:42 +0200

    Revisione dummy player

--- esempi/hangman/agents.py
+++ esempi/hangman/agents.py
@@ -51,4 +51,51 @@ class DummyPlayer(AutomaticPlayer):
             else:
                 if self.game.score <= 0:
                     return False, self.game.score
-        
+
+class LessDummyPlayer(AutomaticPlayer):
+    
+    def __init__(self, game: Hangmann):
+        super().__init__(game=game)
+        self.asked_chars = set()
+        self.freq = self.count_chars()
+        self.freq = sorted([(x, y) for x, y in self.freq.items()], key=lambda: -x[1])
+    
+    def count_chars(self):
+        freq = {}
+        for word in self.game.words:
+            for c in word:
+                if c in freq.keys():
+                    freq[c] += 1
+                else:
+                    freq[c] = 1
+        return freq
+    
+    def policy(self):
+        return np.random.choice(['L', 'P'])
+    
+    def guess_char(self):
+        candidate = [(c, f) for c, f in self.freq if c not in self.asked_chars]
+        char = candidate[0][0]
+        self.asked_chars.add(char)
+        return char
+    
+    def guess_word(self):
+        return np.random.choice(self.game.words)
+    
+    def play(self, max_iterations: int = 1000):
+        for it in range(max_iterations):
+            win = False
+            p = self.policy()
+            if p == 'L':
+                c = self.guess_char()
+                self.game.char_guess(character=c)
+            elif p == 'P':
+                w = self.guess_word()
+                win = self.game.try_word(word=w)
+            else:
+                pass
+            if win:
+                return win, self.game.score 
+            else:
+                if self.game.score <= 0:
+                    return False, self.game.score
